dc25baa5ab1c4533802ed42b0c38adebbb40911e
mahsanshoaib1 <mahsanshoaib@protonmail.com>
2023-08-17 21:52:28 +0500

    what is the use of import, and dir function in python

---
+++ day_19.py
@@ -0,0 +1,21 @@
+                                # import
+# used to import libraries
+
+import pandas as pd # as is used to give a nick name to the library
+from math import sqrt
+
+import numpy as np
+
+
+a = sqrt(64)
+print(a)
+
+
+arr = np.arange(0,10,1, np.float16)
+print(arr)
+
+
+                            # dir  (is used to print all the functions in a library)
+
+import math
+print(dir(math))
