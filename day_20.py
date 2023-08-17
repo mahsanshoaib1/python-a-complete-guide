57d2e1268230d72366a49ebd4beffbc076e45de2
mahsanshoaib1 <mahsanshoaib@protonmail.com>
2023-08-17 21:53:58 +0500

    use of __name__ ,  and '__main__'

---
+++ day_20.py
@@ -0,0 +1,19 @@
+def welcom():
+    print("This is the main file of day 20.")
+    
+def ahsan():
+    print("My name is Ahsan. I'm probably a good boy.")
+    
+def intro():
+    print("this is not an intro function.")
+    
+# welcom()
+
+# solve function calling problem
+
+if __name__ == "__main__":
+    '''--name-- means from which value the file is running
+        yeh function is file mein run hoga, lekin main mein sirf tab run hoga jab call kia jayy ga
+    '''
+    
+    welcom()
